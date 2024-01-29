c4736e9f9a1a41e49c192f9adda4a50b6cad57ed
HERO-ILATI <hero@ilati.com>
2024-01-30 02:33:49 +0900

    token lock fix

--- contracts/MetaStockToken.sol
+++ contracts/MetaStockToken.sol
@@ -14,6 +14,16 @@ contract MetaStockToken is
     Ownable,
     ERC20Permit
 {
+    struct LockInfo {
+        uint256 releaseTime;
+        uint256 balance;
+    }
+
+    mapping(address => LockInfo[]) internal lockInfo;
+
+    event Lock(address indexed holder, uint256 value, uint256 releaseTime);
+    event Unlock(address indexed holder, uint256 value);
+
     constructor(
         address initialOwner
     )
@@ -36,6 +46,106 @@ contract MetaStockToken is
         _mint(to, amount);
     }
 
+    function lock(
+        address holder,
+        uint256 amount,
+        uint256 releaseTime
+    ) public onlyOwner {
+        require(balanceOf(holder) >= amount, "Balance is too small.");
+        require(
+            block.timestamp <= releaseTime,
+            "TokenTimelock: release time is before current time"
+        );
+
+        // _balances[holder] = _balances[holder].sub(amount);
+        // super._burn(holder, amount)
+        _update(holder, address(0), amount);
+        lockInfo[holder].push(LockInfo(releaseTime, amount));
+        emit Lock(holder, amount, releaseTime);
+    }
+
+    function unlock(address holder, uint256 i) public onlyOwner {
+        require(i < lockInfo[holder].length, "Locked balance does not exists.");
+
+        // _balances[holder] = _balances[holder].add(
+        //     lockInfo[holder][i].balance
+        // );
+        // super._mint(holder, lockInfo[holder][i].balance)
+        _update(address(0), holder, lockInfo[holder][i].balance);
+        emit Unlock(holder, lockInfo[holder][i].balance);
+        lockInfo[holder][i].balance = 0;
+
+        if (i != lockInfo[holder].length - 1) {
+            lockInfo[holder][i] = lockInfo[holder][lockInfo[holder].length - 1];
+        }
+        // lockInfo[holder].length--;
+        lockInfo[holder].pop();
+    }
+
+    function _releaseLock(address holder) internal {
+        for (uint256 i = 0; i < lockInfo[holder].length; i++) {
+            if (lockInfo[holder][i].releaseTime <= block.timestamp) {
+                // _balances[holder] = _balances[holder].add(
+                //     lockInfo[holder][i].balance
+                // );
+                _update(address(0), holder, lockInfo[holder][i].balance);
+                emit Unlock(holder, lockInfo[holder][i].balance);
+                lockInfo[holder][i].balance = 0;
+
+                if (i != lockInfo[holder].length - 1) {
+                    lockInfo[holder][i] = lockInfo[holder][
+                        lockInfo[holder].length - 1
+                    ];
+                    i--;
+                }
+                // lockInfo[holder].length--;
+                lockInfo[holder].pop();
+            }
+        }
+    }
+
+    function lockCount(address holder) public view returns (uint256) {
+        return lockInfo[holder].length;
+    }
+
+    function lockState(
+        address holder,
+        uint256 idx
+    ) public view returns (uint256, uint256) {
+        return (
+            lockInfo[holder][idx].releaseTime,
+            lockInfo[holder][idx].balance
+        );
+    }
+
+    function transferWithLock(
+        address to,
+        uint256 value,
+        uint256 releaseTime
+    ) public onlyOwner returns (bool) {
+        address owner = _msgSender();
+        if (to == address(0)) {
+            revert ERC20InvalidReceiver(address(0));
+        }
+        if (balanceOf(owner) < value) {
+            revert ERC20InsufficientBalance(owner, balanceOf(owner), value);
+        }
+
+        // require(value <= balanceOf(owner), "Not enough balance");
+        require(
+            block.timestamp <= releaseTime,
+            "TokenLockError: The release time is before the current time."
+        );
+
+        // _balances[owner] = _balances[owner].sub(_value);
+        _update(owner, address(0), value);
+        lockInfo[to].push(LockInfo(releaseTime, value));
+        emit Transfer(owner, to, value);
+        emit Lock(to, value, releaseTime);
+
+        return true;
+    }
+
     // The following functions are overrides required by Solidity.
 
     function _update(
@@ -48,5 +158,23 @@ contract MetaStockToken is
 
     function decimals() public pure override returns (uint8) {
         return 6;
+    }
+
+    function transfer(
+        address to,
+        uint256 value
+    ) public override returns (bool) {
+        address holder = _msgSender();
+        _releaseLock(holder);
+        return super.transfer(to, value);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) public override returns (bool) {
+        _releaseLock(from);
+        return super.transferFrom(from, to, value);
     }
 }
